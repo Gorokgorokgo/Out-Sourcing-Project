a8f4e2a81b33715432e5f31dcc4327df7a1a6f6e
wlsql852 <wlsql852@naver.com>
2024-09-22 22:50:31 +0900

    fix : customerId를 AuthUser에서 받아올 수 있도록 수정

--- src/main/java/com/sparta/outsourcing/repository/CustomerRepository.java
+++ src/main/java/com/sparta/outsourcing/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.sparta.outsourcing.repository;
 import com.sparta.outsourcing.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByCustomerId(Long customerId);
 }
