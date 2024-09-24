98a601ad199b07c1e16f4efbeeb605f16aadc9dc
wlsql852 <wlsql852@naver.com>
2024-09-25 02:50:32 +0900

    fix : 통합 검색 로직 수정

--- src/main/java/com/sparta/outsourcing/service/SearchService.java
+++ src/main/java/com/sparta/outsourcing/service/SearchService.java
@@ -6,7 +6,7 @@ import com.sparta.outsourcing.dto.search.SearchLatestResponseDto;
 import com.sparta.outsourcing.dto.search.SearchRequestDto;
 import com.sparta.outsourcing.dto.search.SearchResponsDto;
 import com.sparta.outsourcing.dto.store.StoreDetailResponseDto;
-import com.sparta.outsourcing.dto.store.StoreResponseDto;
+import com.sparta.outsourcing.dto.store.StoreSimpleResponseDto;
 import com.sparta.outsourcing.entity.Customer;
 import com.sparta.outsourcing.entity.Menu;
 import com.sparta.outsourcing.entity.Search;
@@ -42,7 +42,7 @@ public class SearchService {
         if(keyword == null) throw new WrongInputException("keyword가 입력되지 않았습니다.");
         if(address == null || address.split(" ").length < 3) throw new WrongInputException("address를 다시 입력해주세요.");
         //가게 검색 결과
-        List<StoreResponseDto> storeResult = new ArrayList<>();
+        List<StoreSimpleResponseDto> storeResult = new ArrayList<>();
         //메뉴 검색 결과
         List<StoreDetailResponseDto> menuResult = new ArrayList<>();
         //메뉴 -> 가게 리스트
@@ -55,53 +55,59 @@ public class SearchService {
         //가게 검색
         //가게 이름이 일치하는 경우 찾기
         List<Store> storeList = storeRepository.findAllByStoreNameContains(keyword);
+        //store중 개업한 경우만 확인
+        storeList = storeList.stream().filter(store -> store.isStoreStatus()==true).toList();
         //가게가 없으면 검색된 가게 결과 없다고 오류
         if(storeList.isEmpty()) log.info("통합검색 :: 가게 검색 결과 없음");
-        //결과가 15개 이상이면 같은 동에 있는 것만 전달
-        if(storeList.size() > 4) {
-            List<Store> storeDongList = storeList.stream().filter(store -> store.getAddress().contains(addressDong)).toList();
-            storeResult.addAll(storeDongList.stream().map(StoreResponseDto::new).toList());
-            //검색 결과가 너무 적으면 구까지 넓혀서 검색
-            if(storeDongList.size() < 2) {
-                List<Store> storeGuList = storeList.stream()
-                        .filter(store -> store.getAddress().contains(addressGu))
-                        .filter(store -> !storeDongList.contains(store))
-                        .toList();
-                storeResult.addAll(storeGuList.stream().map(StoreResponseDto::new).toList());
+
+        //검색 결과 중 같은 동에 있는 것을 위주로 확인
+        List<Store> storeDongList = storeList.stream().filter(store -> store.getAddress().contains(addressDong)).toList();
+        storeResult.addAll(storeDongList.stream().map(StoreSimpleResponseDto::new).toList());
+        //결과가 4개 이하이면 구까지 넓혀서 검색
+        if(storeResult.size() < 5) {
+            //같은 구에 있는 가게 찾기(앞의 중복되는 가게는 지우기)
+            List<Store> storeGuList = getGuStore (storeList, addressGu,storeDongList);
+            storeResult.addAll(storeGuList.stream().map(StoreSimpleResponseDto::new).toList());
+            //그래도 검색 결과가 너무 적으면 전체로 검색
+            if(storeResult.size() < 5) {
+                //전체 중에 앞의 중복되는 가게를 제외한 검색 결과
+                List<Store> storeLastList = getAllStore (storeList, storeGuList,storeDongList);
+                storeResult.addAll(storeLastList.stream().map(StoreSimpleResponseDto::new).toList());
             }
         }
-        else  storeResult = storeList.stream().map(StoreResponseDto::new).toList();
+        //검색 결과를 5개만 출력
+        if(storeResult.size() > 5) storeResult = new ArrayList<>(storeResult.subList(0, 5));
 
-        //주소 앞부분 찾기
+
+        //메뉴 검색
         //메뉴 이름에 keword가 들어간 menu찾기
         List<Menu> menuList = menuRepository.findAllByMenuNameContains(keyword);
+        //가게가 개업한 곳만 확인
+        menuList = menuList.stream().filter(menu -> menu.getStore().isStoreStatus()==true).toList();
         //검색 결과 없으면 없다고 말함
         if(menuList.isEmpty()) log.info("통합검색 :: 메뉴 검색 결과 없음");
 
-        //각 메뉴를 가게의 객체로 변환하여 storeList에 합치기
-//        storeList.addAll(menuList.stream().map(m->m.getStore()).toList());
-        //검색 결과가 15개 넘으면 가게가 동에 있는걸로 한정
-        if(menuList.size() > 4) {
-            List<Menu> menuDongList = menuList.stream().filter(menu -> menu.getStore().getAddress().contains(addressDong)).toList();
-            storeMenuList= menuDongList.stream().map(Menu::getStore).toList();
-            menuListAdd(menuResult, storeMenuList, menuDongList);
-            //너무 적으면 가게가 구로 있는 데이터까지 확장
-            if(menuDongList.size() < 2) {
-                List<Menu> menuGuList = menuList.stream()
-                        .filter(menu -> menu.getStore().getAddress().contains(addressGu))
-                        .filter(menu -> !menuDongList.contains(menu))
-                        .toList();
-                storeMenuList = menuGuList.stream().map(Menu::getStore).toList();
-                menuListAdd(menuResult, storeMenuList, menuGuList);
+        //같은 동에 있는 메뉴를 먼저 설정
+        List<Menu> menuDongList = menuList.stream().filter(menu -> menu.getStore().getAddress().contains(addressDong)).toList();
+        storeMenuList= menuDongList.stream().map(Menu::getStore).toList();
+        menuListAdd(menuResult, storeMenuList, menuDongList);
+        //검색 결과가 너무 적으면 구로 넓혀서 검색
+        if(menuResult.size() < 5) {
+            //가게가 같은 구에 위차한 메뉴 찾기(앞과 중복되는 결과는 제외)
+            List<Menu> menuGuList = getGuMenu(menuList, addressGu, menuDongList);
+            //각 메뉴를 가게로 바꾸기
+            storeMenuList = menuGuList.stream().map(Menu::getStore).toList();
+            //결과값을 responseDto형태로 바꿈
+            menuListAdd(menuResult, storeMenuList, menuGuList);
+            //그래도 너무 적으면 전체 검색 (앞과 중복되는 결과는 제외)
+            if(menuResult.size() < 5) {
+                List<Menu> menuLastList = getAllMenu(menuList, menuGuList, menuDongList);
+                storeMenuList = menuLastList.stream().map(Menu::getStore).toList();
+                menuListAdd(menuResult, storeMenuList, menuLastList);
             }
-            return new SearchAllResponseDto(storeResult, menuResult);
         }
-        //처음 검색 결과가 15개보다 적으면 모두 결과물로 변환
-        storeMenuList = menuList.stream().map(Menu::getStore).toList();
-        for(int i = 0; i < storeMenuList.size(); i++) {
-            menuResult.add(new StoreDetailResponseDto(menuList.get(i), storeMenuList.get(i)));
-        }
-        //가게와 메뉴 검색 결과를 하나로 만들어 responseDto로 최종 전달
+        //검색 결과를 5개만 출력
+        if(menuResult.size() > 5) menuResult = new ArrayList<>(menuResult.subList(0, 5));
         return new SearchAllResponseDto(storeResult, menuResult);
     }
 
@@ -124,5 +130,33 @@ public class SearchService {
         for(int i = 0; i < storeMenuList.size(); i++) {
             menuResult.add(new StoreDetailResponseDto(menuList.get(i), storeMenuList.get(i)));
         }
+    }
+
+    private List<Store> getGuStore (List<Store> storeList, String addressGu,List<Store> storeDongList) {
+        return storeList.stream()
+                .filter(store -> store.getAddress().contains(addressGu))
+                .filter(store -> !storeDongList.contains(store))
+                .toList();
+    }
+
+    private List<Store> getAllStore (List<Store> storeList, List<Store> storeGuList,List<Store> storeDongList) {
+        return storeList.stream()
+                .filter(store -> !storeDongList.contains(store))
+                .filter(store -> !storeGuList.contains(store))
+                .toList();
+    }
+
+    private List<Menu> getGuMenu(List<Menu> menuList, String addressGu, List<Menu> menuDongList) {
+        return menuList.stream()
+                .filter(menu -> menu.getStore().getAddress().contains(addressGu))
+                .filter(menu -> !menuDongList.contains(menu))
+                .toList();
+    }
+
+    private List<Menu> getAllMenu(List<Menu> menuList, List<Menu> menuGuList, List<Menu> menuDongList) {
+        return menuList.stream()
+                .filter(menu -> !menuDongList.contains(menu))
+                .filter(menu -> !menuGuList.contains(menu))
+                .toList();
     }
 }
