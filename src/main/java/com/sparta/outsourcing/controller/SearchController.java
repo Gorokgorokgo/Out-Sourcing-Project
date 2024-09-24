b3af7946c49766a19b6e5fd7959d1558d9a58ada
wlsql852 <wlsql852@naver.com>
2024-09-24 17:39:33 +0900

    fix : SearchController 각 함수에 ResponseEntity추가

--- src/main/java/com/sparta/outsourcing/controller/SearchController.java
+++ src/main/java/com/sparta/outsourcing/controller/SearchController.java
@@ -8,6 +8,7 @@ import com.sparta.outsourcing.dto.search.SearchRequestDto;
 import com.sparta.outsourcing.dto.search.SearchResponsDto;
 import com.sparta.outsourcing.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,37 +17,28 @@ import org.springframework.web.bind.annotation.*;
 public class SearchController {
     private final SearchService searchService;
 
+
     /**
-     * 사용자 주소를 중심으로 해당 키워드의 가게나 메뉴 찾기
-     * @param keyword : 검색할 키워드
-     * @param address : 사용자 주소(나중에 AuthUser로 바뀌어야함)
-     * @return 검색결과 Dto
-     */
-    @GetMapping("/all/user")
-    public SearchAllResponseDto searchAllUser (@RequestParam String keyword, @RequestParam String address) {
-        return searchService.searchAll(keyword, address);
-    }
-    /**
-     * 사용자가 지정한 주소를 중심으로 해당 키워드의 가게나 메뉴 찾기
+     * 사용자가 지정한 주소 또는 로그인 정보의 주소를 중심으로 해당 키워드의 가게나 메뉴 찾기
      * @param keyword : 검색할 키워드
      * @param address : 사용자 지정한 주소
      * @return 검색결과 Dto
      */
     @GetMapping("/all/address")
-    public SearchAllResponseDto searchAllAddress (@RequestParam String keyword, @RequestParam String address) {
-        return searchService.searchAll(keyword, address);
+    public ResponseEntity<SearchAllResponseDto> searchAllAddress (@RequestParam String keyword, @RequestParam String address) {
+        return ResponseEntity.ok(searchService.searchAll(keyword, address));
     }
 
 
     @PostMapping("")
-    public SearchResponsDto createSearch (@Auth AuthUser authUser, @RequestBody SearchRequestDto requestDto) {
-        return searchService.createSearch(authUser, requestDto);
+    public ResponseEntity<SearchResponsDto> createSearch (@Auth AuthUser authUser, @RequestBody SearchRequestDto requestDto) {
+        return ResponseEntity.ok(searchService.createSearch(authUser, requestDto));
 
     }
 
     @GetMapping("/latest")
-    public SearchLatestResponseDto searchLatest (@Auth AuthUser authUser) {
-        return searchService.searchLatest(authUser);
+    public ResponseEntity<SearchLatestResponseDto> searchLatest (@Auth AuthUser authUser) {
+        return ResponseEntity.ok(searchService.searchLatest(authUser));
     }
 
 }
