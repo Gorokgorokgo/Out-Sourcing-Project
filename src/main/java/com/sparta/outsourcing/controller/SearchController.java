f934e435210b7ec5a1e822f8eae306e93b677072
wlsql852 <wlsql852@naver.com>
2024-09-24 10:30:54 +0900

    feat : 로그인 정보로 주소 검색/사용자가 직접 지정한 주소로도 검색으로 경로 분리

--- src/main/java/com/sparta/outsourcing/controller/SearchController.java
+++ src/main/java/com/sparta/outsourcing/controller/SearchController.java
@@ -14,8 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
     private final SearchService searchService;
 
-    @GetMapping("/all")
-    public SearchResponseDto searchAll (@RequestParam String keyword, @RequestParam String address) {
+    /**
+     * 사용자 주소를 중심으로 해당 키워드의 가게나 메뉴 찾기
+     * @param keyword : 검색할 키워드
+     * @param address : 사용자 주소(나중에 AuthUser로 바뀌어야함)
+     * @return 검색결과 Dto
+     */
+    @GetMapping("/all/user")
+    public SearchResponseDto searchAllUser (@RequestParam String keyword, @RequestParam String address) {
         return searchService.searchAll(keyword, address);
     }
+    /**
+     * 사용자가 지정한 주소를 중심으로 해당 키워드의 가게나 메뉴 찾기
+     * @param keyword : 검색할 키워드
+     * @param address : 사용자 지정한 주소
+     * @return 검색결과 Dto
+     */
+    @GetMapping("/all/address")
+    public SearchResponseDto searchAllAddress (@RequestParam String keyword, @RequestParam String address) {
+        return searchService.searchAll(keyword, address);
+    }
+
+
 }
